e73253a52aecf29a31622b180254aebc05aad578
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-08-17 13:43:50 +0200

    update stomp test

--- plugins/outputs/stomp/stomp_test.go
+++ plugins/outputs/stomp/stomp_test.go
@@ -16,15 +16,18 @@ func TestiConnectAndWrite(t *testing.T) {
 	}
 
 	var url = testutil.GetLocalHost() + ":61613"
+	s, err := serializers.NewJsonSerializer(10)
+	require.NoError(t, err)
+
 	st := &STOMP{
 		Host:      url,
 		Username:  "",
 		Password:  "",
 		QueueName: "test_queue",
 		SSL:       false,
-		serialize: serializers.NewJsonSerializer(),
+		serialize: s,
 	}
-	err := st.Connect()
+	err = st.Connect()
 	require.NoError(t, err)
 
 	err = st.Write(testutil.MockMetrics())
